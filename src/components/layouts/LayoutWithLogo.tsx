e034537dbf251503c17eb176c9a3bcfe72d605b5
seungjinhan <hanblues@gmail.com>
2023-08-21 09:44:17 +0800

    code

--- src/components/layouts/LayoutWithLogo.tsx
+++ src/components/layouts/LayoutWithLogo.tsx
@@ -34,18 +34,21 @@ const LayoutWithLogo = ({
     setWidthSize(window.innerWidth);
 
     const user = ElseUtils.getLocalstorageUser();
-    const obj = new ChannelService();
-    obj.loadScript();
-    obj.boot({
-      pluginKey: "1c92aeb0-97cd-4046-bbb2-1b3beb594511", // fill your plugin key
-      profile: {
-        // fill user's profile
-        name: user.email, // fill user's name
-        mobileNumber: user.phone, // fill user's mobile number
-        landlineNumber: "USER_LANDLINE_NUMBER", // fill user's landline number
-        CUSTOM_VALUE_1: user.name, // custom property
-      },
-    });
+
+    console.log(user);
+
+    // const obj = new ChannelService();
+    // obj.loadScript();
+    // obj.boot({
+    //   pluginKey: "1c92aeb0-97cd-4046-bbb2-1b3beb594511", // fill your plugin key
+    //   profile: {
+    //     // fill user's profile
+    //     name: user.email, // fill user's name
+    //     mobileNumber: user.phone, // fill user's mobile number
+    //     landlineNumber: "USER_LANDLINE_NUMBER", // fill user's landline number
+    //     CUSTOM_VALUE_1: user.name, // custom property
+    //   },
+    // });
     // obj.openChat("76824", "tst");
   }, []);
 
